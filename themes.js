77ef3efcf5b872950de3288abb1db8e6bac3d055
Astromations <80211195+Astromations@users.noreply.github.com>
2023-06-03 18:27:03 +0000

    Create themes.js

---
+++ themes.js
@@ -0,0 +1,9 @@
+(() => {
+    const script = document.createElement("SCRIPT");
+    script.setAttribute("type", "text/javascript");
+    script.setAttribute(
+      "src",
+      "https://astromations.github.io/Hazy/hazy.js"
+    );
+    document.head.appendChild(script);
+  })();
