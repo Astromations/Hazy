d197e3823a9ee28c21a229577279c2eed2a7f08d
Astromations <80211195+Astromations@users.noreply.github.com>
2023-06-04 09:47:53 +0000

    Delete themes.js

--- themes.js
+++
@@ -1,9 +0,0 @@
-(() => {
-    const script = document.createElement("SCRIPT");
-    script.setAttribute("type", "text/javascript");
-    script.setAttribute(
-      "src",
-      "https://astromations.github.io/Hazy/hazy.js"
-    );
-    document.head.appendChild(script);
-  })();
